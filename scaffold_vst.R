57b40f8df028606ad38d37a40a5b23a427e4a78b
malthomas <54588527+malthomas@users.noreply.github.com>
2019-09-17 11:24:26 -0600

    Add files via upload

--- scaffold_vst.R
+++ scaffold_vst.R
@@ -1,20 +1,11 @@
-pkg <- c("stringr", "reshape2",  "dplyr", "ggplot2",  "magrittr")
-
-new.pkg <- pkg[!(pkg %in% installed.packages())]
-
-if (length(new.pkg)) {
-  install.packages(new.pkg)
-}
-
-library(stringr)
-library(reshape2)
-suppressPackageStartupMessages(library(dplyr))
-library(ggplot2)
-
 setwd("C:/Users/malth/CNV/Ratios")
 folder <- "C:/Users/malth/CNV/Ratios"
+
 CN <- list.files(path = folder, recursive = TRUE, full.names = FALSE,)
 CN                                                     
 
 CN_read <- lapply(CN, function(x) {read.table(file = x, header = T, sep ="\t")})
 CN_df <- do.call("cbind", lapply(CN_read, as.data.frame))
+
+CN_clean <- data.frame(CN_df$Chromosome, CN_df$Start, CN_df$Sb1, CN_df$Sb2, CN_df$Sb3, CN_df$Sb4, CN_df$Sb5, CN_df$Sb6, CN_df$Sb8, CN_df$Sb9, CN_df$Sb10, CN_df$Sb11, CN_df$Sb12, CN_df$Sb13, CN_df$Sb14, CN_df$Sb15, CN_df$Sb16, CN_df$Sb18, CN_df$Sb20, CN_df$Sb21, CN_df$Sb22, CN_df$Sb23, CN_df$Sb24, CN_df$Sb25, CN_df$Sb26, CN_df$Sb27, CN_df$Sb28, CN_df$Sb29, CN_df$Sb34)
+CN_clean
