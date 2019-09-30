d7195c89f2e07483b50433ef2ccb26d0f528fa75
malthomas <54588527+malthomas@users.noreply.github.com>
2019-09-29 21:24:21 -0600

    Add files via upload

--- scaffold_vst.R
+++ scaffold_vst.R
@@ -1,37 +1,43 @@
-setwd("C:/Users/malth/CNV/Ratios")
-folder <- "C:/Users/malth/CNV/Ratios"
-
-#Create Data Frame 
-CN <- list.files(path = folder, recursive = TRUE, full.names = FALSE,)
-
-CN_read <- lapply(CN, function(x) {read.table(file = x, header = T, sep ="\t")})
-
-CN_df <- do.call("cbind", lapply(CN_read, as.data.frame))
-
-CN_clean <- data.frame(CN_df$Chromosome, CN_df$Start, CN_df$Sb1, CN_df$Sb2, CN_df$Sb3, CN_df$Sb4, CN_df$Sb5, CN_df$Sb6, CN_df$Sb8, CN_df$Sb9, CN_df$Sb10, CN_df$Sb11, CN_df$Sb12, CN_df$Sb13, CN_df$Sb14, CN_df$Sb15, CN_df$Sb16, CN_df$Sb18, CN_df$Sb20, CN_df$Sb21, CN_df$Sb22, CN_df$Sb23, CN_df$Sb24, CN_df$Sb25, CN_df$Sb26, CN_df$Sb27, CN_df$Sb28, CN_df$Sb29, CN_df$Sb34)
-
-write.csv(CN_clean, file = 'C:/Users/malth/CNV/CN_clean.csv')
-
-#Variance Data Frames
-library(tidyverse)
-library(readr)
-CN_var <- read_csv("C:/Users/malth/CNV/CN_var.csv")
-
-mnt <- c('Sb1', 'Sb2', 'Sb3', 'Sb4', 'Sb5', 'Sb6', 'Sb8', 'Sb9', 'Sb10', 'Sb11', 'Sb12', 'Sb13', 'Sb14', 'Sb15', 'Sb16', 'Sb18')
-cst <- c('Sb20', 'Sb21', 'Sb22', 'Sb23', 'Sb24', 'Sb25', 'Sb26', 'Sb27', 'Sb28', 'Sb29', 'Sb34')
-total <- c('Sb1', 'Sb2', 'Sb3', 'Sb4', 'Sb5', 'Sb6', 'Sb8', 'Sb9', 'Sb10', 'Sb11', 'Sb12', 'Sb13', 'Sb14', 'Sb15', 'Sb16', 'Sb18', 'Sb20', 'Sb21', 'Sb22', 'Sb23', 'Sb24', 'Sb25', 'Sb26', 'Sb27', 'Sb28', 'Sb29', 'Sb34')
-
-CN_total <- CN_var %>%
-            pivot_wider(id_cols = 'Chromosome', 'Start', names_from =   , values_from = )
+library("tidyverse")
+library("stringr")
+library("data.table")
+setwd("C:/Users/malth/Documents/CNV/Ratios")
+folder <- "C:/Users/malth/Documents/CNV/Ratios"
 
 
-CN_mnt <- CN_var %>%
-            select(-cst)
+#List Sb files 
+  CN_list <-  list.files(path = folder, recursive = TRUE, full.names = FALSE,)
+#Read each Sb file in the list
+  CN_read <-  lapply(CN_list, read.table, header=T, sep="\t", stringsAsFactors = F)
+#Combine all Sb files into a single dataframe
+  CN_df <-  bind_cols(CN_read) %>% #bind dataframe by columns
+            select(Chromosome, Start, starts_with("Sb")) %>% #select variables of interest
+            unite("window", Chromosome:Start, remove = T) #create row identifiers
+# Make it pretty
+  total <- c('Sb1', 'Sb2', 'Sb3', 'Sb4', 'Sb5', 'Sb6', 'Sb8', 'Sb9', 'Sb10', 'Sb11', 'Sb12', 'Sb13', 'Sb14', 'Sb15', 'Sb16', 'Sb18','Sb20', 'Sb21', 'Sb22', 'Sb23', 'Sb24', 'Sb25', 'Sb26', 'Sb27', 'Sb28', 'Sb29', 'Sb34')
+  CN_clean <- CN_df %>%
+              data.frame(row.names = 'window') %>%
+              select(total) %>%
+              t()
+# Total variance
+  CN_total <- CN_clean %>%
+              transmute_all(funs(var))
 
-CN_cst <- CN_var %>%
-  select(-mnt)
+# Montane samples variance  
+  mnt <- c('Sb1', 'Sb2', 'Sb3', 'Sb4', 'Sb5', 'Sb6', 'Sb8', 'Sb9', 'Sb10', 'Sb11', 'Sb12', 'Sb13', 'Sb14', 'Sb15', 'Sb16', 'Sb18')
+  
+# Coastal samples variance 
+  cst <- c('Sb20', 'Sb21', 'Sb22', 'Sb23', 'Sb24', 'Sb25', 'Sb26', 'Sb27', 'Sb28', 'Sb29', 'Sb34')
 
 
+write.csv(CN_variance, file = 'C:/Users/malth/Docuemnts/CNV/CN_variance.csv')
+
+
+##NEXT STEPS
+#calculate variance of each column (window) and put the value in a new data table
+#repeate for cst and mnt 
+#calculate vst for each window
+#manhattan plot
 
           
               
