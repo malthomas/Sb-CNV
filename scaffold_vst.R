d7b82fdcbd20b9d08f4b21844cb7c54f9be344be
malthomas <54588527+malthomas@users.noreply.github.com>
2019-10-07 10:24:36 -0600

    Add files via upload

--- scaffold_vst.R
+++ scaffold_vst.R
@@ -11,7 +11,7 @@ folder <- "C:/Users/malth/Documents/CNV/Ratios"
   
 
   
-##TOTAL VARIANCE  
+##TOTAL DATAFRAME  
 # Create dataframe
   CN_df <- bind_cols(CN_read) %>% #bind columns from Sb files
             select(Chromosome, Start, starts_with("Sb"))%>% #select variables of interest
@@ -22,17 +22,10 @@ folder <- "C:/Users/malth/Documents/CNV/Ratios"
   window <- CN_df$window #remember row id
   CN_total <- as.data.frame(t(CN_df[,-1])) #transpose dataframe, exclude 1st colunmn
   colnames(CN_total) <- window #create column names  
-  write.csv(CN_total, file = 'C:/Users/malth/Documents/CNV/CN_total.csv') #save dataframe
   
-# Calculate variance
-  CN_total <- as.tbl(CN_total) #convert dataframe to tibble 
-  var_total <- summarise_at(CN_total, vars(1:208889), var) #apply the variance function to each column, output new data frame
-  var_total <- t(var_total) #transpose dataframe, output matrix
-  colnames(var_total) <- c("Total Variance") #name variable column
+  write.csv(CN_total, file = 'C:/Users/malth/Documents/CNV/CN_total.csv') #save dataframe
 
-    
-
-##MONTANE VARIANCE  
+##MONTANE DATAFRAME  
 # Create dataframe
   mnt <- c('Sb1', 'Sb2', 'Sb3', 'Sb4', 'Sb5', 'Sb6', 'Sb8', 'Sb9', 'Sb10', 'Sb11', 'Sb12', 'Sb13', 'Sb14', 'Sb15', 'Sb16', 'Sb18')
   CN_mnt <- select(CN_df, window, one_of(mnt))
@@ -41,17 +34,10 @@ folder <- "C:/Users/malth/Documents/CNV/Ratios"
   window <- CN_mnt$window #remember row id
   CN_mnt <- as.data.frame(t(CN_mnt[,-1])) #transpose dataframe, exclude 1st colunmn
   colnames(CN_mnt) <- window #create column names  
+  
   write.csv(CN_mnt, file = 'C:/Users/malth/Documents/CNV/CN_mnt.csv') #save dataframe
   
-# Calculate variance
-  CN_mnt <- as.tbl(CN_mnt) #convert dataframe to tibble 
-  var_mnt <- summarise_at(CN_mnt, vars(1:208889), var) #apply the variance function to each column, output new data frame
-  var_mnt <- t(var_mnt) #transpose dataframe, output matrix
-  colnames(var_mnt) <- c("Total Variance") #name variable column
-  
-  
-  
-##COASTAL VARIANCE 
+##COASTAL DATAFRAME 
 # Create dataframe
   cst <- c('Sb20', 'Sb21', 'Sb22', 'Sb23', 'Sb24', 'Sb25', 'Sb26', 'Sb27', 'Sb28', 'Sb29', 'Sb34')
   CN_cst <- select(CN_df, window, one_of(cst))
@@ -60,18 +46,36 @@ folder <- "C:/Users/malth/Documents/CNV/Ratios"
   window <- CN_cst$window #remember row id
   CN_cst <- as.data.frame(t(CN_cst[,-1])) #transpose dataframe, exclude 1st colunmn
   colnames(CN_cst) <- window #create column names  
+  
   write.csv(CN_cst, file = 'C:/Users/malth/Documents/CNV/CN_cst.csv') #save dataframe
   
-# Calculate variance
+
+##CALCULATE VARIANCE  
+# total variance
+  CN_total <- as.tbl(CN_total) #convert dataframe to tibble 
+  var_total <- summarise_at(CN_total, vars(1:208889), var) #apply the variance function to each column, output new data frame
+  var_total <- t(var_total) #transpose dataframe, output matrix
+  colnames(var_total) <- c("Total Variance") #name variable column
+
+  
+# montane variance
+  CN_mnt <- as.tbl(CN_mnt) #convert dataframe to tibble 
+  var_mnt <- summarise_at(CN_mnt, vars(1:208889), var) #apply the variance function to each column, output new data frame
+  var_mnt <- t(var_mnt) #transpose dataframe, output matrix
+  colnames(var_mnt) <- c("Total Variance") #name variable column
+  
+  
+# coastal variance
   CN_cst <- as.tbl(CN_cst) #convert dataframe to tibble 
   var_cst <- summarise_at(CN_cst, vars(1:208889), var) #apply the variance function to each column, output new data frame
   var_cst <- t(var_cst) #transpose dataframe, output matrix
   colnames(var_cst) <- c("Total Variance") #name variable column
   
 
+##COMBINE VARIANCE DATAFRAMES
+
+
 ##CALCULATE VST
-#Combine variance dataframes
-  
 
 
 ##MANHATTAN PLOT
